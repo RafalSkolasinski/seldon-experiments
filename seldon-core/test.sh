58c5e25a26aa31cadc51a233bddb28c3b062b7ba
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2020-02-18 18:15:07 +0000

    further improvement on seldon-core makefile

--- seldon-core/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-helm install seldon-core seldon-core-operator \
---repo https://storage.googleapis.com/seldon-charts \
---set ambassador.enabled=true \
---set executor.enabled=true \
---namespace seldon-system
