831fd21dafc4220017507d186adc2b17f6318a43
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2020-05-05 10:10:11 +0100

    add misc graphs

---
+++ misc/Model.py
@@ -0,0 +1,12 @@
+import logging
+import numpy as np
+import os
+
+name = os.environ.get("NAME", "unkown")
+
+class Model():
+
+    def predict(self, X, feature_names=[]):
+        logging.warning(X)
+        logging.warning(feature_names)
+        return np.append(X, [name])
